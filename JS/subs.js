95a4e8fd711a9b219c9752bad01502950713437c
nicolasvaudagna <nico.vaudagna14@gmail.com>
2021-10-18 12:46:27 -0300

    city name validated

--- JS/subs.js
+++ JS/subs.js
@@ -6,13 +6,13 @@ window.onload = function(){
     var age = document.getElementById('age')
     var phoneNumber = document.getElementById('phone-number')
     var address = document.getElementById('address')
-    var location = document.getElementById('location')
+    var ciry = document.getElementById('city')
     var zipCode = document.getElementById('zip-code')
     var idNumber = document.getElementById('id-number')
 
 // VAlIDATE NAME
-    completeName.addEventListener('blur', validateName)
-    function validateName(e){
+    completeName.addEventListener('blur', validateName1)
+    function validateName1(e){
         if (e.target.value.length < 6){
             e.target.nextElementSibling.innerHTML = "Name must contain at least 6 letters"
         }
@@ -20,26 +20,26 @@ window.onload = function(){
             e.target.nextElementSibling.innerHTML = "Name must contain at least 2 words"
         }
     }
-    completeName.addEventListener('focus', validateName1)
-    function validateName1(e){
+    completeName.addEventListener('focus', validateName2)
+    function validateName2(e){
         e.target.nextElementSibling.innerHTML = " "
     }
 
 // VALIDATE EMAIL
-    email.addEventListener('blur', validateemail)
-    function validateemail(e){
+    email.addEventListener('blur', validateemail1)
+    function validateemail1(e){
         if (!e.target.value.includes("@"&&".")){
             e.target.nextElementSibling.innerHTML = "It must be a valid email"
         }
     }
-    email.addEventListener('focus', validateemail1)
-    function validateemail1(e){
+    email.addEventListener('focus', validateemail2)
+    function validateemail2(e){
         e.target.nextElementSibling.innerHTML = " "
     }
    
 // VALIDATE PASSWORD
-    password1.addEventListener('blur', validatePassword)
-    function validatePassword(e){
+    password1.addEventListener('blur', validatePassword1)
+    function validatePassword1(e){
         if (e.target.value.length < 8){
             e.target.nextElementSibling.innerHTML = "Password must contain at least 8 characters"
         }
@@ -50,20 +50,20 @@ window.onload = function(){
         //     e.target.nextElementSibling.innerHTML = "Password must contain letters and numbers"
         // }  
     }
-    password1.addEventListener('focus', validatePassword1)
-    function validatePassword1(e){
+    password1.addEventListener('focus', validatePassword2)
+    function validatePassword2(e){
         e.target.nextElementSibling.innerHTML = " "
     }
 
 // VALIDATE REPEATPASSWORD
-    repeatPassword.addEventListener('blur', validateRepeatPass)
-    function validateRepeatPass(e){
+    repeatPassword.addEventListener('blur', validateRepeatPass1)
+    function validateRepeatPass1(e){
         // if ((e.target.value) != (password1.target.value)){
         //     e.target.nextElementSibling.innerHTML = "Password must be the same"
         // }
     }
-    repeatPassword.addEventListener('focus', validateRepeatPass)
-    function validateRepeatPass(e){
+    repeatPassword.addEventListener('focus', validateRepeatPass2)
+    function validateRepeatPass2(e){
         e.target.nextElementSibling.innerHTML = " "
     }
 
@@ -83,8 +83,8 @@ window.onload = function(){
     }
 
 // VALIDATE PHONE NUMBER revisar
-    phoneNumber.addEventListener('blur', validatePhoneNumber)
-    function validatePhoneNumber(e){
+    phoneNumber.addEventListener('blur', validatePhoneNumber1)
+    function validatePhoneNumber1(e){
         if (e.target.value.length < 7){
             e.target.nextElementSibling.innerHTML = "Phone number must be at least 7 digits long"
         }
@@ -96,15 +96,16 @@ window.onload = function(){
     function validatePhoneNumber2(e){
         e.target.nextElementSibling.innerHTML = " "
     }
+
 // VALIDATE ADDRESS
-    address.addEventListener('blur', validateAddress)
-    function validateAddress(e){
+    address.addEventListener('blur', validateAddress1)
+    function validateAddress1(e){
         if (e.target.value.length < 5){
             e.target.nextElementSibling.innerHTML = "Address must have at least 5 characters"
         }
-        else if (!e.target.value.includes(Number)){
-            e.target.nextElementSibling.innerHTML = "Address must contain at least a word and a number"
-        }
+        // else if (!e.target.value.includes(Number)){
+        //     e.target.nextElementSibling.innerHTML = "Address must contain at least a word and a number"
+        // }
         else if (!e.target.value.includes(" ")){
             e.target.nextElementSibling.innerHTML = "Address must contain at least two elements"
         }
@@ -112,6 +113,12 @@ window.onload = function(){
     address.addEventListener('focus', validateAddress2)
     function validateAddress2(e){
         e.target.nextElementSibling.innerHTML = " "
+    }
+
+// VALIDATE LOCATION
+    location.addEventListener('blur', validateLocation1)
+    function validateLocation1(e){
+        if (e.target.value.length < 3)
     }
 
 
